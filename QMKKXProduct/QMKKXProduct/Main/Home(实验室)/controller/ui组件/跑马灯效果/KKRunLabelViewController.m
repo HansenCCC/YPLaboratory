a2ea35f8dd7ae8af265bc38df099c04f231f9eb0
程恒盛 <2534550460@qq.com>
2020-07-15 18:12:12 +0800

    程恒盛：跑马灯优化

--- QMKKXProduct/QMKKXProduct/Main/Home(实验室)/controller/ui组件/跑马灯效果/KKRunLabelViewController.m
+++ QMKKXProduct/QMKKXProduct/Main/Home(实验室)/controller/ui组件/跑马灯效果/KKRunLabelViewController.m
@@ -10,7 +10,6 @@
 #import "KKRunLabel.h"
 
 @interface KKRunLabelViewController ()
-@property (strong, nonatomic) KKRunLabel *runLabel;
 
 @end
 
@@ -19,10 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"跑马灯效果";
-    self.runLabel = [[KKRunLabel alloc] init];
-    self.runLabel.titleLabel.font = AdaptedBoldFontSize(23);
-    self.runLabel.titleLabel.text = @"你 真 的 是 个 大 帅 比 ！";
-    [self.view addSubview:self.runLabel];
+    //
+    [self setupRunLabel:AdaptedBoldFontSize(40.f)];
+    [self setupRunLabel:AdaptedFontSize(40.f)];
+    [self setupRunLabel:AdaptedBoldFontSize(30.f)];
+    [self setupRunLabel:AdaptedFontSize(30.f)];
+    [self setupRunLabel:AdaptedBoldFontSize(20.f)];
+    [self setupRunLabel:AdaptedFontSize(20.f)];
+    [self setupRunLabel:AdaptedBoldFontSize(15.f)];
+    [self setupRunLabel:AdaptedFontSize(15.f)];
+    [self setupRunLabel:AdaptedBoldFontSize(10.f)];
+    [self setupRunLabel:AdaptedFontSize(10.f)];
+}
+- (KKRunLabel *)setupRunLabel:(UIFont *)font{
+    KKRunLabel *label = [[KKRunLabel alloc] init];
+    label.titleLabel.font = font;
+    label.titleLabel.text = @"你 真 的 是 个 大 帅 比 ！";
+    [self.view addSubview:label];
+    return label;
 }
 - (void)viewWillLayoutSubviews{
     [super viewWillLayoutSubviews];
@@ -33,9 +46,15 @@
     }
     bounds = UIEdgeInsetsInsetRect(bounds,insets);
     CGRect f1 = bounds;
-    f1.origin.y = AdaptedWidth(200.f);
-    f1.size = [self.runLabel sizeThatFits:CGSizeMake(bounds.size.width - 2 * AdaptedWidth(50.f), 0)];
-    f1.origin.x = (bounds.size.width - f1.size.width)/2.0;
-    self.runLabel.frame = f1;
+    f1.origin.y = SafeAreaTopHeight;
+    f1.size.height = AdaptedWidth(0.f);
+    for (UIView *view in self.view.subviews) {
+        if ([view isKindOfClass:[KKRunLabel class]]) {
+            f1.origin.y = CGRectGetMaxY(f1) + AdaptedWidth(20.f);
+            f1.size = [view sizeThatFits:CGSizeMake(bounds.size.width - 2 * AdaptedWidth(50.f), 0)];
+            f1.origin.x = (bounds.size.width - f1.size.width)/2.0;
+            view.frame = f1;
+        }
+    }
 }
 @end
